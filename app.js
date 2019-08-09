4373f0f81c81b42ae1176231b2099ee4ec2cd9c0
mmorales056 <mmorales@misena.edu.co>
2019-08-09 07:31:27 -0500

    Commit inicial

---
+++ app.js
@@ -0,0 +1,24 @@
+const argv = require('./config/yargs').argv;
+var colors = require('colors');
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite)
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado ${archivo}`))
+            .catch(err => console.log(err));
+        break;
+
+    default:
+        console.log("Comando no definido");
+        break;
+}
+
+
+//let parametro = argv[2];
+//let base = parametro.split('=')[1];
